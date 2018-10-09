3886e88d06f02e8ae004cd627453af90b2449ebb
rap2h <raphaelht@gmail.com>
2018-10-09 23:17:16 +0200

    Updates

---
+++ precache-manifest.c14b5c05a1f9455d0771a389dce1cb23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f8d2d2f3ee0ce799856",
+    "url": "/react-fairy-tale-emoji/static/js/runtime~main.9f8d2d2f.js"
+  },
+  {
+    "revision": "343ead610b6ec4f79cfa",
+    "url": "/react-fairy-tale-emoji/static/js/main.343ead61.chunk.js"
+  },
+  {
+    "revision": "7f42b2c06cc50180a4a3",
+    "url": "/react-fairy-tale-emoji/static/js/1.7f42b2c0.chunk.js"
+  },
+  {
+    "revision": "343ead610b6ec4f79cfa",
+    "url": "/react-fairy-tale-emoji/static/css/main.ce8dae37.chunk.css"
+  },
+  {
+    "revision": "1b8fec6700d4f5a2fb24598c9e85ec8e",
+    "url": "/react-fairy-tale-emoji/index.html"
+  }
+];
